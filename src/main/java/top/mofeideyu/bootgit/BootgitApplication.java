028fce88ea6df28013ef1e0c9900268e75b75e48
mofeideyu <mofei2914376031@163.com>
2022-06-09 23:23:59 +0800

    init

---
+++ src/main/java/top/mofeideyu/bootgit/BootgitApplication.java
@@ -0,0 +1,13 @@
+package top.mofeideyu.bootgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootgitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootgitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/mofeideyu/bootgit/BootgitApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mofeideyu.bootgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootgitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
